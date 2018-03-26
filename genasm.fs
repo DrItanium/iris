ce87e3e8238ec1bd4b7f9acbaed99748b4a75200
Josh Scoggins <jscoggins@parasoft.com>
2018-03-26 11:26:30 -0700

    do not overwrite basic operators like swap and such

--- genasm.fs
+++ genasm.fs
@@ -1,12 +1,12 @@
 enum-start
-#define X(c, t, w) enum-next : c ( -- n ) literal ; 
-#define FirstX(c, t, w) : c ( -- n ) literal ;
+#define X(c, t, w) enum-next : Asm ## c ( -- n ) literal ; 
+#define FirstX(c, t, w) : Asm ## c ( -- n ) literal ;
 #include "Opcodes.def"
 #undef X
 #undef FirstX
 enum-done
 
-#define X(c, t, w) : w ( args* -- n ) t c bitwise-oru ;
+#define X(c, t, w) : ! ## w ( args* -- n ) t Asm ## c bitwise-oru ;
 #define FirstX(c, t, w) X(c, t, w) 
 #include "Opcodes.def"
 #undef FirstX
