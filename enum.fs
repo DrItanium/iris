69ce56b6e7ddd04b29c747affe5fb5f4916e0dab
Josh Scoggins <jscoggins@parasoft.com>
2018-03-26 13:23:46 -0700

    Merged the enum.fs with the basics in the forth interpreter

--- enum.fs
+++
@@ -1,7 +0,0 @@
-( enum like support )
-( duplicate the top of the stack after incrementing )
-: enum-start ( -- 0 0 ) 0 dup ;
-: enum-next  ( n1 -- n2 n2 ) 1 + dup ;
-: enum-done  ( n1 -- ) drop ;
-
-close-input-file
