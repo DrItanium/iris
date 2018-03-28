90bb9c2261d491f865957cd7217c647c96b32987
Josh Scoggins <jscoggins@parasoft.com>
2018-03-28 10:00:03 -0700

    More code in the test assembler

--- test0.fs
+++ test0.fs
@@ -3,13 +3,36 @@
     close-input-file must be the last word in the file, anything after it will not
     even be parsed so it is like a giant comment block there
 )
+: .data16 ( value -- ) dup variable? if @ then mask-immediate16 asm<< ;
+deflabel ShutdownProcessor
+deflabel OperationAdd
+deflabel OperationSubtract
+deflabel JumpTableStart
+deflabel OperationMultiply
 {asm 
-    .code
-    !nop
+.code
     0 zero !set
     zero sp2 !move
     7FFF# sp !set
-    r0 !terminateExecution
+ShutdownProcessor label-here
+    args1 !terminateExecution
+OperationAdd {func 
+   args3 !add 
+   func}
+OperationSubtract {func
+   args3 !sub
+   func}
+OperationMultiply {func
+   args3 !mul
+   func}
+.data 
+FF00# .org
+JumpTableStart label-here
+    ShutdownProcessor .data16
+    OperationAdd .data16
+    OperationSubtract .data16
+    OperationMultiply .data16
+    0 .data16
 asm}
 close-input-file
 
