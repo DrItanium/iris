b3e3f01a5d9b3e98e10fcf2e715331b1f89ed87f
Josh Scoggins <jscoggins@parasoft.com>
2018-04-24 14:38:14 -0700

    Fully implemented the two new instructions

--- Core.cc
+++ Core.cc
@@ -325,6 +325,44 @@ namespace iris {
             }
         }
     }
+    DefExec(SaveGroupOfRegisters) {
+        // extract the bits from the immediate and use it when calling the 
+        // push operation
+        Core::Push tmp;
+        tmp._args.dest = op._args.dest;
+        // shift by three to get the correct offset
+        auto base = (op._args.imm & 0b11111) << 3;
+        auto mask = (op._args.imm & 0b1111111100000000) >> 8;
+        for (auto index = base; index < (base + 8); ++index) {
+            // start at the smallest value and work your way up
+            if ((mask & 0b1) != 0) {
+                tmp._args.src = SourceRegister(index);
+                perform(tmp);
+            }
+            mask = mask >> 1;
+        }
+    }
+    DefExec(RestoreGroupOfRegisters) {
+        // extract the bits from the immediate and use it when calling the pop
+        // operation
+        // 
+        // unlike the pushg operation, this one will do things a tad 
+        // differently
+        Core::Pop tmp;
+        tmp._args.src = op._args.dest; // the destination becomes the source
+        auto base = ((op._args.imm & 0b11111) << 3); // go over by one
+        auto mask = (op._args.imm & 0b1111111100000000);
+        static constexpr decltype(mask) upperMostMask = 0b1000'0000'0000'0000;
+        for (auto index = base + 7; index >= base; --index) {
+            // we shift to the left instead of right and use the mask to determine
+            // if we should pop a value off
+            if ((upperMostMask & mask) != 0) {
+                tmp._args.dest = DestinationRegister(index);
+                perform(tmp);
+            }
+            mask = mask << 1;
+        }
+    }
 #undef DefExec
     void Core::installIODevice(Core::IODevice dev) {
         _io.emplace_back(dev);
