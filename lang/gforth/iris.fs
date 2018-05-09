a46a8943a49ce519f697019a5b34d80daa729a3b
Josh Scoggins <jscoggins@parasoft.com>
2018-05-09 12:10:35 -0700

    Auto dump to file when outputting instructions

--- lang/gforth/iris.fs
+++ lang/gforth/iris.fs
@@ -26,50 +26,17 @@ variable mloc \ current memory location
 : 1reg ( dest -- value ) dest-reg ;
 : 2reg ( src dest -- value ) 1reg swap src-reg or ;
 : 3reg ( src2 src dest -- value ) 2reg swap src2-reg or ;
-: 4reg ( src3 src2 src dest -- value ) 3reg swap src3-reg or ;
 : imm16 ( imm16 -- value ) addr16 16 lshift ;
-: imm12 ( imm12 -- value ) addr12 20 lshift ;
-: imm6 ( imm6 -- value ) addr6 26 lshift ;
 
-: 1reg-with-imm ( imm16 dest -- value )
-  1reg swap imm16 or ;
-: 2reg-with-imm ( imm12 src dest -- value )
-  2reg swap imm12 or ;
-: 3reg-with-imm ( imm6 src2 src dest -- value )
-  3reg swap imm6 or ;
 : cconstant ( byte "name" -- ) create c, does> c@ ;
 : xop& ( n a -- k ) c@ or ;
-: inst-1reg ( opcode-index "name" -- )
-  create dup c, 1+ \ embed opcode
-  does> >r 
-        1reg
-        r> xop& ;
-: inst-2reg ( opcode-index "name" -- )
-  create dup c, 1+ \ embed opcode
-  does> >r
-        2reg
-        r> xop& ;
-: inst-3reg ( opcode-index "name" -- )
-  create dup c, 1+ \ embed opcode
-  does> >r 
-        3reg 
-		r> xop& ;
-: inst-4reg ( opcode-index "name" -- )
-  create dup c, 1+ \ embed opcode
-  does> >r
-        4reg
-        r> xop& ;
-: inst-1reg-with-imm ( opcode-index "name" -- )
-  create dup c, 1+ \ embed opcode
-  does> >r
-        1reg-with-imm
-        r> xop& ;
 0 constant RegisterValueSpace
 1 constant MemorySpace
 2 constant CoreMemorySpace
 3 constant InstructionSpace 
 4 constant LabelSpace
 5 constant IndirectInstructionSpace
+6 constant IndirectMemorySpace
 
 
 : linker-entry ( kind address value -- n ) 
@@ -88,10 +55,136 @@ variable mloc \ current memory location
 : registers} ( n -- ) drop ;
 : register: dup cconstant 1+ ;
 : {opcode ( -- 0 ) {registers ;
+: opcode: dup cconstant 1+ ;
 : opcode} ( n -- ) registers} ;
+
+{opcode
+opcode: #add 
+opcode: #sub 
+opcode: #mul 
+opcode: #div 
+opcode: #rem 
+opcode: #shl 
+opcode: #shr 
+opcode: #and 
+opcode: #or 
+opcode: #not 
+opcode: #xor 
+opcode: #nand 
+opcode: #nor
+opcode: #min
+opcode: #max
+opcode: #eq
+opcode: #neq
+opcode: #lt
+opcode: #gt
+opcode: #le
+opcode: #ge
+opcode: #set
+opcode: #ld
+opcode: #st
+opcode: #push
+opcode: #pop
+opcode: #ldc
+opcode: #stc
+opcode: #ldio
+opcode: #stio
+opcode: #br
+opcode: #brl
+opcode: #bcr
+opcode: #bcrl
+opcode: #ueq
+opcode: #uneq
+opcode: #ult
+opcode: #ugt
+opcode: #ule
+opcode: #uge
+opcode: #uand
+opcode: #uor
+opcode: #unot
+opcode: #uxor
+opcode: #unand
+opcode: #unor
+opcode: #umin
+opcode: #umax
+opcode: #uadd
+opcode: #usub
+opcode: #umul
+opcode: #udiv
+opcode: #urem
+opcode: #ushl
+opcode: #ushr
+opcode: #readtok
+opcode: #number
+opcode}
 
 \ registers
 set-current \ go back
+
+
+RegisterValueSpace def-space-entry register-entry
+MemorySpace def-space-entry memory-entry
+\ CoreMemorySpace def-space-entry core-entry
+InstructionSpace def-space-entry instruction-entry
+LabelSpace def-space-entry label-entry 
+IndirectInstructionSpace def-space-entry indirect-instruction-entry 
+IndirectMemorySpace def-space-entry indirect-memory-entry
+: <<linker ( entry id -- ) 
+  hex
+  dup >r
+  nout
+  s" " r> write-line throw ;
+: <<inst ( inst id -- ) 
+  >r
+  loc@ swap instruction-entry
+  r> <<linker 
+  loc2+ \ each instruction is two entries
+  ;
+: <<iinst ( inst id -- ) 
+  >r
+  loc@ swap indirect-instruction-entry
+  r> <<linker 
+  loc2+ \ each instruction is two entries
+  ;
+: <<mem ( value id -- )
+  >r
+  loc@ swap memory-entry
+  r> <<linker loc1+ ;
+: <<imem ( value id -- )
+  >r
+  loc@ swap indirect-memory-entry
+  r> <<linker loc1+ ;
+: <<register ( index value id -- ) 
+  >r
+  register-entry 
+  r> <<linker ; 
+: <<label ( index id -- ) 
+  >r
+  loc@ swap label-entry
+  r> <<linker ;
+\ labels must be defined ahead of time before first reference
+: reset-labels ( -- ) 0 labelIndex ! ;
+: deflabel ( "name" -- ) create labelIndex @ addr32 , labelIndex @ 1+ labelIndex ! does> @ ;
+
+
+: .org ( n -- ) loc! ;
+
+variable CurrentAssemblyFile
+
+: curasm@ ( -- file ) CurrentAssemblyFile @ ;
+: curasm! ( n -- ) CurrentAssemblyFile ! ;
+: clearasm ( -- ) 0 curasm! ;
+: <<imminst ( n set -- ) curasm@ swap over <<iinst <<inst ;
+: <<inst ( n -- ) curasm@ <<inst ;
+: .label ( label -- ) curasm@ <<label ;
+: .data16 ( n -- ) curasm@ swap addr16 swap <<mem ;
+: .data32 ( n -- ) curasm@ swap addr32 swap <<inst ;
+: .idata32 ( n -- ) curasm@ swap addr16 swap <<imem ;
+: .register ( index value -- ) curasm@ <<register ;
+: {asm ( path -- ) 
+  w/o create-file throw curasm! 
+  reset-labels ;
+: asm} ( -- ) curasm@ close-file throw clearasm ;
 
 {registers
 register: r0
@@ -174,96 +267,113 @@ r11 cconstant at2
 r12 cconstant at3
 r13 cconstant io
 r14 cconstant ci \ core index number
-
 : sanity-check-opcode ( op expected -- ) <> ABORT" opcode does not match expected!" ;
-{opcode
-inst-3reg add, 
+: inst-1reg ( opcode-index "name" -- )
+  create dup c, 1+ \ embed opcode
+  does> >r 
+        1reg
+        r> xop& <<inst ;
+: inst-2reg ( opcode-index "name" -- )
+  create dup c, 1+ \ embed opcode
+  does> >r
+        2reg
+        r> xop& <<inst ;
+: inst-3reg ( opcode-index "name" -- )
+  create dup c, 1+ \ embed opcode
+  does> >r 
+        3reg 
+		r> xop& <<inst ;
+
+#add inst-3reg add, 
 : move, ( src dest -- n ) zero swap add, ;
 : nop, ( -- n ) zero zero zero add, ;
 : -> ( src dest -- n ) move, ;
-inst-3reg sub, 
-inst-3reg mul, 
-inst-3reg div, 
-inst-3reg rem, 
-inst-3reg shl, 
-inst-3reg shr, 
-inst-3reg and, 
-inst-3reg or, 
-inst-2reg not, 
-inst-3reg xor, 
-inst-3reg nand, 
-inst-3reg nor,
-inst-3reg min,
-inst-3reg max,
-inst-3reg eq,
-inst-3reg neq,
-inst-3reg lt,
-inst-3reg gt,
-inst-3reg le,
-inst-3reg ge,
-inst-1reg-with-imm set,
-\ sanity check
+\ constant tagging version
+\ #, is a constant version
+: #, ( -- 0 ) 0 ; 
+\ !, is an indirect instruction
+: !, ( -- 1 ) 1 ;
 
-0 zero set, 21 sanity-check-opcode 
-: $-> ( imm dest -- n ) set, ;
-: $->at0 ( imm -- n ) at0 $-> ;
-: $->at1 ( imm -- n ) at1 $-> ;
-: $->at0-3arg ( imm a b -- at0 a b n ) 
-  rot $->at0 >r 
-  at0 ( a b at0 ) -rot ( at0 a b ) 
-  r> ( at0 a b n ) ;
+#sub inst-3reg sub, 
+#mul inst-3reg mul, 
+#div inst-3reg div, 
+#rem inst-3reg rem, 
+#shl inst-3reg shl, 
+#shr inst-3reg shr, 
+#and inst-3reg and, 
+#or inst-3reg or, 
+#not inst-2reg not, 
+#xor inst-3reg xor, 
+#nand inst-3reg nand, 
+#nor inst-3reg nor,
+#min inst-3reg min,
+#max inst-3reg max,
+#eq inst-3reg eq,
+#neq inst-3reg neq,
+#lt inst-3reg lt,
+#gt inst-3reg gt,
+#le inst-3reg le,
+#ge inst-3reg ge,
+: set, ( imm imm-type dest -- )
+  1reg #set
+  or ( imm it v ) rot ( it v imm ) 
+  addr16 16 lshift or
+  swap ( v it )
+  0= if ( constant ) <<inst else <<iinst endif ;
+: #set, ( imm dest -- ) #, swap set, ;
+: !set, ( imm dest -- ) !, swap set, ;
 
-: $->at0-2arg ( imm b -- at0 b n )
-  swap ( b imm )
-  $->at0 at0 -rot ( at0 b n ) ;
-: $->at0-1arg ( imm -- at0 ) $->at0 at0 ;
- inst-2reg ld,
- inst-2reg st,
- inst-2reg push,
- inst-2reg pop,
- inst-2reg ldc,
- inst-2reg stc,
- inst-2reg ldio,
- inst-2reg stio,
- inst-1reg br,
- inst-2reg brl,
- inst-2reg bcr,
- inst-3reg bcrl,
- inst-3reg ueq,
- inst-3reg uneq,
- inst-3reg ult,
- inst-3reg ugt,
- inst-3reg ule,
- inst-3reg uge,
- inst-3reg uand,
- inst-3reg uor,
- inst-2reg unot,
- inst-3reg uxor,
- inst-3reg unand,
- inst-3reg unor,
- inst-3reg umin,
- inst-3reg umax,
- inst-3reg uadd,
- inst-3reg usub,
- inst-3reg umul,
- inst-3reg udiv,
- inst-3reg urem,
- inst-3reg ushl,
- inst-3reg ushr,
- inst-2reg readtok,
- inst-3reg number,
-opcode}
+: $-> ( imm id dest -- n ) set, ;
+: $->at0 ( imm id -- n ) at0 $-> ;
+: $->at1 ( imm id -- n ) at1 $-> ;
+: $->at0-3arg ( imm id a b -- at0 a b ) >r >r $->at0 at0 r> r> swap ;
+: $->at0-2arg ( imm id b -- at0 b )
+  -rot ( b imm id )
+  $->at0 ( b )
+  at0 swap ( at0 b ) ;
+#ld inst-2reg ld,
+#st inst-2reg st,
+#push inst-2reg push,
+#pop inst-2reg pop,
+#ldc inst-2reg ldc,
+#stc inst-2reg stc,
+#ldio inst-2reg ldio,
+#stio inst-2reg stio,
+#br inst-1reg br,
+#brl inst-2reg brl,
+#bcr inst-2reg bcr,
+#bcrl inst-3reg bcrl,
+#ueq inst-3reg ueq,
+#uneq inst-3reg uneq,
+#ult inst-3reg ult,
+#ugt inst-3reg ugt,
+#ule inst-3reg ule,
+#uge inst-3reg uge,
+#uand inst-3reg uand,
+#uor inst-3reg uor,
+#unot inst-2reg unot,
+#uxor inst-3reg uxor,
+#unand inst-3reg unand,
+#unor inst-3reg unor,
+#umin inst-3reg umin,
+#umax inst-3reg umax,
+#uadd inst-3reg uadd,
+#usub inst-3reg usub,
+#umul inst-3reg umul,
+#udiv inst-3reg udiv,
+#urem inst-3reg urem,
+#ushl inst-3reg ushl,
+#ushr inst-3reg ushr,
+#readtok inst-2reg readtok,
+#number inst-3reg number,
 
 : def2argi ( "name" "op" -- )
   create ' , 
   does> ( imm dest -- n set-op )
   >r \ stash the address for now
-  $->at0-2arg
+  $->at0-2arg 
   r> \ get the top back
-  swap
-  >r \ stash the set op for now
   @ execute \ execute the stashed operation
-  r> \ restore the top
   ;
 
 : def3argi ( "name" "op" -- )
@@ -272,16 +382,13 @@ opcode}
   >r \ stash the address for now
   $->at0-3arg
   r> \ get the top back
-  swap
-  >r \ put the new value back onto the stack
   @ execute \ we should now have the argument correctly setup
-  r> \ get the set back as well
   ;
 
-: b, ( imm -- n s ) $->at0 >r at0 br, r> ;
-: bl, ( imm lr -- n s ) swap $->at0 >r at0 brl, r> ;
-: bc, ( imm cond -- n s ) swap $->at0 >r at0 bcr, r> ;
-: bcl, ( imm link cond -- n s ) rot $->at0 >r at0 bcrl, r> ;
+: b, ( imm id -- ) $->at0 at0 br, ;
+: bl, ( imm id lr -- ) -rot $->at0 at0 brl, ;
+: bc, ( imm id cond -- ) -rot $->at0 at0 bcr, ;
+: bcl, ( imm id link cond -- ) >r >r $->at0 r> r> swap at0 bcrl, ;
 def3argi addi, add,
 def3argi subi, sub,
 def3argi muli, mul,
@@ -317,56 +424,4 @@ def3argi ulti, ult,
 def3argi ugti, ugt,
 def3argi ulei, ule,
 def3argi ugei, uge,
-
-RegisterValueSpace def-space-entry register-entry
-MemorySpace def-space-entry memory-entry
-CoreMemorySpace def-space-entry core-entry
-InstructionSpace def-space-entry instruction-entry
-LabelSpace def-space-entry label-entry 
-IndirectInstructionSpace def-space-entry indirect-instruction-entry 
-: <<linker ( entry id -- ) 
-  hex
-  dup >r
-  nout
-  s" " r> write-line throw ;
-: <<inst ( inst id -- ) 
-  >r
-  loc@ swap instruction-entry
-  r> <<linker 
-  loc2+ \ each instruction is two entries
-  ;
-: <<iinst ( inst id -- ) 
-  >r
-  loc@ swap indirect-instruction-entry
-  r> <<linker 
-  loc2+ \ each instruction is two entries
-  ;
-: <<mem ( value id -- )
-  >r
-  loc@ swap memory-entry
-  r> <<linker loc1+ ;
-: <<core ( inst id -- )
-  >r 
-  loc@ swap core-entry
-  r> <<linker loc1+ ;
-: <<register ( index value id -- ) 
-  >r
-  register-entry 
-  r> <<linker ; 
-: <<label ( index id -- ) 
-  >r
-  loc@ swap label-entry
-  r> <<linker ;
-\ labels must be defined ahead of time before first reference
-: reset-labels ( -- ) 0 labelIndex ! ;
-: deflabel ( "name" -- ) create labelIndex @ addr32 , labelIndex @ 1+ labelIndex ! does> @ ;
-
-
-: .label ( label id -- ) <<label ; 
-: .org ( n -- ) loc! ;
-: .data16 ( n id -- ) swap addr16 swap <<mem ;
-: .data32 ( n id -- )  swap addr32 swap <<inst ;
-\ : dumpable-inst ( "name" "name2" -- ) 
-\  create ' does> swap >r @ execute r> <<inst ;
-
 previous
