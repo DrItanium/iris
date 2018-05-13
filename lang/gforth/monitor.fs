1452afb4a49d62f99df26cabadd735b7cf89f7e2
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-05-12 18:20:55 -0700

    Added ?sysinit variable into a register

--- lang/gforth/monitor.fs
+++ lang/gforth/monitor.fs
@@ -5,6 +5,7 @@ s" monitor.o" {asm
 0xD000 constant dictionary-start
 0xE800 constant data-stack-start
 0xF000 constant call-stack-start
+0xA000 constant routines-start
 deflabel InputRoutine
 deflabel TerminateExecutionRoutine
 deflabel Restart
@@ -13,11 +14,25 @@ r63 constant &InputRoutine
 r62 constant &TerminateExecutionRoutine
 r61 constant &Restart
 r60 constant dp
-
+r59 constant arg0
+r58 constant arg1
+r57 constant ret0
+r56 constant ?sysinit
+routines-start .org
+TerminateExecutionRoutine .label
+	/dev/terminate-vm #->io
+	arg0 io-write
+	ret, 
+	
 dictionary-start .org
 CoreDictionaryStart .label
 0x0000 .org
 Restart !, &Restart $->
+TerminateExecutionRoutine !, &TerminateExecutionRoutine $->
+InputRoutine !, &InputRoutine $->
+
+
+0xFFFF #, ?sysinit $->
 \ initialization code goes here
 Restart .label
 	data-stack-start #, dsp $->
