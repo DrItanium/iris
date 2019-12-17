e9317a11ffe44fb417e2e26e8143ee451b9eb003
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-12-17 13:06:42 -0800

    Fixed up design choices

--- arduino/sketches/MetroM0Implementation/MetroM0Implementation.ino
+++ arduino/sketches/MetroM0Implementation/MetroM0Implementation.ino
@@ -10,13 +10,10 @@
 #include "iris.h"
 #include "arduino_core.h"
 
-iris::ArduinoCore core;
 constexpr auto SD_CS = 4;
 constexpr auto TFT_CS = 10;
 constexpr auto TFT_DC = 8;
 constexpr auto TFT_RST = -1;
-Adafruit_TFTShield18 ss;
-Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RST);
 constexpr auto LowerFRAM = 5;
 constexpr auto UpperFRAM = 6;
 constexpr auto CodeSectionEnable = 5;
@@ -24,6 +21,9 @@ constexpr auto DataSectionEnable = 6;
 constexpr auto StackSectionEnable = 6;
 template<int pin>
 using CableSelectHolder = bonuspin::DigitalPinHolder<pin, LOW, HIGH>;
+iris::ArduinoCore core;
+Adafruit_TFTShield18 ss;
+Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RST);
 // FM25V20A FRAM CHIPS 
 namespace fram {
     
@@ -48,55 +48,43 @@ void transferAddress(uint32_t address) {
   SPI.transfer(uint8_t(address));
 }
 
+void sendOpcodeAndAddress(Opcodes opcode, uint32_t address) noexcept {
+    sendOpcode(opcode);
+    transferAddress(address);
+}
+
+void beginFastReadOperation(uint32_t address) noexcept {
+    sendOpcodeAndAddress(Opcodes::FSTRD, address);
+    SPI.transfer(0x00); // dummy byte
+}
+
 template<int csPin>
 uint8_t read8(uint32_t address) {
   CableSelectHolder<csPin> startup;
-  sendOpcode(Opcodes::READ);
-  transferAddress(address);
+  sendOpcodeAndAddress(Opcodes::READ, address);
   return SPI.transfer(0x00);
 }
+
+inline uint16_t getSPI16() noexcept {
+    return static_cast<uint16_t>(SPI.transfer(0x00)) | (static_cast<uint16_t>(SPI.transfer(0x00)) << 8);
+}
+
 
 template<int enablePin>
 uint16_t read16(uint32_t address) {
   CableSelectHolder<enablePin> reader;
-  sendOpcode(Opcodes::FSTRD);
-  transferAddress(address << 1);
-  SPI.transfer(0x00); // send a dummy byte
-  auto lower = static_cast<uint16_t>(SPI.transfer(0x00));
-  auto upper = static_cast<uint16_t>(SPI.transfer(0x00)) << 8;
-  return lower | upper;
+  beginFastReadOperation(address << 1);
+  return getSPI16();
 }
 template<int enablePin>
 uint32_t read32(uint32_t address) {
   CableSelectHolder<enablePin> reader;
-  sendOpcode(Opcodes::FSTRD);
-  //sendOpcode(Opcodes::READ);
-  transferAddress(address << 2);
-  SPI.transfer(0x00); // transfer a dummy byte first
-  auto lowest = static_cast<uint32_t>(SPI.transfer(0x00));
-  auto lower = static_cast<uint32_t>(SPI.transfer(0x00)) << 8;
-  auto higher = static_cast<uint32_t>(SPI.transfer(0x00)) << 16;
-  auto highest = static_cast<uint32_t>(SPI.transfer(0x00)) << 24;
-  return lowest | lower | higher | highest;
+  beginFastReadOperation(address << 2);
+  auto lower = static_cast<uint32_t>(getSPI16());
+  auto upper = static_cast<uint32_t>(getSPI16()) << 16;
+  return lower | upper;
 }
 
-template<int enablePin>
-uint64_t read64(uint64_t address) {
-  CableSelectHolder<enablePin> reader;
-  //sendOpcode(Opcodes::READ);
-  sendOpcode(Opcodes::FSTRD);
-  transferAddress(address << 3);
-  SPI.transfer(0x00); // dummy byte
-  auto a = static_cast<uint64_t>(SPI.transfer(0x00));
-  auto b = static_cast<uint64_t>(SPI.transfer(0x00)) << 8;
-  auto c = static_cast<uint64_t>(SPI.transfer(0x00)) << 16;
-  auto d = static_cast<uint64_t>(SPI.transfer(0x00)) << 24;
-  auto e = static_cast<uint64_t>(SPI.transfer(0x00)) << 32;
-  auto f = static_cast<uint64_t>(SPI.transfer(0x00)) << 40;
-  auto g = static_cast<uint64_t>(SPI.transfer(0x00)) << 48;
-  auto h = static_cast<uint64_t>(SPI.transfer(0x00)) << 56;
-  return a | b | c | d | e | f | g | h;
-}
 template<int csPin>
 void enableWrites() {
   CableSelectHolder<csPin> startup;
@@ -107,52 +95,42 @@ template<int csPin>
 void writeFram(uint32_t address, uint8_t value) { 
   enableWrites<csPin>();
   CableSelectHolder<csPin> startup;
-  // send WREN before any write operation
-  sendOpcode(Opcodes::WRITE);
-  transferAddress(address);
+  sendOpcodeAndAddress(Opcodes::WRITE, address);
   SPI.transfer(value);
+}
+inline void putSPI16(uint16_t value) noexcept {
+    SPI.transfer(uint8_t(value));
+    SPI.transfer(uint8_t(value >> 8));
 }
 template<int csPin>
 void writeFram(uint32_t address, uint16_t value) {
   enableWrites<csPin>();
   CableSelectHolder<csPin> startup;
-  sendOpcode(Opcodes::WRITE);
-  transferAddress(address << 1);
-  SPI.transfer(uint8_t(value));
-  SPI.transfer(uint8_t(value >> 8));
+  sendOpcodeAndAddress(Opcodes::WRITE, address << 1);
+  putSPI16(value);
 }
 template<int csPin>
 void writeFram(uint32_t address, uint32_t value) {
   enableWrites<csPin>();
   CableSelectHolder<csPin> startup;
-  sendOpcode(Opcodes::WRITE);
-  transferAddress(address << 2);
-  SPI.transfer(uint8_t(value));
-  SPI.transfer(uint8_t(value >> 8));
-  SPI.transfer(uint8_t(value >> 16));
-  SPI.transfer(uint8_t(value >> 24));
-}
-template<int csPin>
-void writeFram(uint32_t address, uint64_t value) {
-  enableWrites<csPin>();
-  CableSelectHolder<csPin> startup;
-  sendOpcode(Opcodes::WRITE);
-  transferAddress(address << 3);
-  SPI.transfer(uint8_t(value));
-  SPI.transfer(uint8_t(value >> 8));
-  SPI.transfer(uint8_t(value >> 16));
-  SPI.transfer(uint8_t(value >> 24));
-  SPI.transfer(uint8_t(value >> 36));
-  SPI.transfer(uint8_t(value >> 40));
-  SPI.transfer(uint8_t(value >> 48));
-  SPI.transfer(uint8_t(value >> 56));
-  
+  sendOpcodeAndAddress(Opcodes::WRITE, address << 2);
+  putSPI16(value);
+  putSPI16(value >> 16);
 }
 constexpr auto max8BitAddress = 0x40000;
 constexpr auto max16BitAddress = max8BitAddress >> 1;
 constexpr auto max32BitAddress = max8BitAddress >> 2;
-constexpr auto max64BitAddress = max8BitAddress >> 3;
 } // end namespace fram
+// so there are two fram chips with 256 kbytes each,
+// fram chip 1 is reserved for code memory
+// fram chip 2 is split between data and stack
+// the lower 128 kbytes of chip2 contains data
+// the upper 128 kbytes of chip2 contains stack
+//
+// it may be simpler to think of the chips in terms of words:
+// Chip 2 contains 128 kwords where 1 word = 2 bytes
+// Chip 1 contains 64 kdwords where 1 dword = 2 words = 4 bytes
+// thus chip 2 has two 64 kword sections
 namespace iris {
 LongOrdinal 
 ArduinoCore::loadFromCodeMemory(Address addr) {
@@ -160,14 +138,24 @@ ArduinoCore::loadFromCodeMemory(Address addr) {
 }
 Ordinal 
 ArduinoCore::loadFromDataMemory(Address addr) {
-    return 0;
+    return fram::read16<DataSectionEnable>(addr);
 }
 Ordinal 
 ArduinoCore::loadFromStackMemory(Address addr) {
+    // the read routines disallow unaligned loads and stores
+    //
+    // iris addresses are 16-bit aligned so internally read16 will shift by one
+    // to convert it to an 8-bit aligned address, thus we want to mark the
+    // stack address as being the "upper" part of chip 2
+    static constexpr uint32_t stackBaseAddress = 0b1'0000'0000'0000'0000;
+    uint32_t internalAddress = stackBaseAddress | static_cast<uint32_t>(addr);
+    return fram::read16<StackSectionEnable>(internalAddress);
     return 0;
 }
 Ordinal 
 ArduinoCore::loadFromIOMemory(Address addr) {
+    // io memory is special as it is really just a programmable section which
+    // allows me to wire up all kinds of devices here
     return 0;
 }
 void 
@@ -177,7 +165,7 @@ ArduinoCore::storeToCodeMemory(Address addr, LongOrdinal value) {
 
 void 
 ArduinoCore::storeToDataMemory(Address addr, Ordinal value) {
-
+    
 }
 void 
 ArduinoCore::storeToStackMemory(Address addr, Ordinal value) {
